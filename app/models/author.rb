e2e7c5a4d67b82f64df86b2fecfdf9658c7d6835
LoopFruits <Kensworldc@gmail.com>
2022-04-11 10:35:54 -0400

    completed assignmnet

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,20 @@
 class Author < ApplicationRecord
-end
+    has_many :posts
+
+
+    has_one :profile 
+end 
+
+
+# in our rails console( to check associations)
+
+# old way of creating a new post 
+# new_post = Post.create(author_id: author.id, title: "Web Development for Cats")
+
+
+# #new way
+# author = Author.first
+# new_post = author.posts.create(title: "Web Development for Cats")
+
+# author.posts
+# #=> [#<Post @id=1>, #<Post @id=5>]
